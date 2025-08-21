383a60b18534f69d224d20da3a8856523add1e45
Joel Martin <64128660+joel-mrtn@users.noreply.github.com>
2025-08-21 13:50:08 +0200

    Initial commit

---
+++ src/bin/blink-builtin/main.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use rp_pico as bsp;
+
+use bsp::entry;
+use bsp::hal::{
+    Watchdog,
+    clocks::{Clock, init_clocks_and_plls},
+    pac,
+    sio::Sio,
+};
+use embedded_hal::digital::StatefulOutputPin;
+
+const DELAY: u32 = 1000;
+
+#[entry]
+fn main() -> ! {
+    let mut p = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(p.WATCHDOG);
+    let sio = Sio::new(p.SIO);
+
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        p.XOSC,
+        p.CLOCKS,
+        p.PLL_SYS,
+        p.PLL_USB,
+        &mut p.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let pins = bsp::Pins::new(p.IO_BANK0, p.PADS_BANK0, sio.gpio_bank0, &mut p.RESETS);
+
+    let mut led_pin = pins.led.into_push_pull_output();
+
+    loop {
+        led_pin.toggle().unwrap();
+        delay.delay_ms(DELAY);
+    }
+}
