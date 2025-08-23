a88d456de4dc64e26cee9c3f2e17da3d641b7484
Joel Martin <64128660+joel-mrtn@users.noreply.github.com>
2025-08-24 01:12:14 +0200

    Add buzzer active project

---
+++ src/bin/buzzer-active/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+use rp_pico as bsp;
+
+use bsp::entry;
+use bsp::hal::{pac, sio::Sio};
+use embedded_hal::digital::{InputPin, OutputPin};
+
+#[entry]
+fn main() -> ! {
+    let mut p = pac::Peripherals::take().unwrap();
+    let sio = Sio::new(p.SIO);
+
+    let pins = bsp::Pins::new(p.IO_BANK0, p.PADS_BANK0, sio.gpio_bank0, &mut p.RESETS);
+
+    let mut buzzer = pins.gpio15.into_push_pull_output();
+    let mut button = pins.gpio16.into_pull_down_input();
+
+    loop {
+        if button.is_low().unwrap() {
+            buzzer.set_high().unwrap();
+        } else {
+            buzzer.set_low().unwrap();
+        }
+    }
+}
